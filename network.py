2608def2697ed64cf35ad896a137a046c628e0de
Jake Ryan <jwr32@psu.edu>
2020-12-18 11:08:22 -0500

    Accurate training to sin wave and half-frequency sin wave

--- network.py
+++ network.py
@@ -1,5 +1,6 @@
 import random
 import numpy as np
+from numpy.testing._private.utils import GetPerformanceAttributes
 from make_plot import *
 
 '''
@@ -18,9 +19,11 @@ class RNN (object):
         self.n = n #Input Nodes
         self.p = p #Hidden Layer Nodes
         self.m = m # Output Layer Nodes
-        self.v = [[random.uniform(-0.5, 0.5) for j in range(p)] for i in range(n + 1)]
-        self.w = [[random.uniform(-0.5, 0.5) for j in range(m)] for i in range(p + 1)]
-        self.u = [[random.uniform(-0.5, 0.5) for j in range(p)] for i in range(p + 1)]
+        self.v = [[random.uniform(-2, 2) for j in range(p)] for i in range(n + 1)]
+        self.w = [[random.uniform(-2, 2) for j in range(m)] for i in range(p + 1)]
+        # Hidden-to-hidden recurrent weights
+        self.u = [random.uniform(-2, 2) for i in range(p)]
+        self.h = [0 for i in range(p)]
 
     def nguyen_widrow(self):
         scale = 0.7*np.power(self.p, 1/self.n)
@@ -35,28 +38,25 @@ class RNN (object):
     def sigmoid(self, x):
         x = np.clip(x, -1000, 1000)
         # x = x/10
-        return (1/(1 + np.exp(-x))) - 1
-    
+        return (2/(1 + np.exp(-x))) - 1
+
+    # Expects x to already have been put through the sigmoid function
+    def sigmoid_prime(self, x):
+        return .5*(1 + x)*(1 - x)
+
     def normalize(self, x, x_i):
         # Normalize the data to within [-1, 1]
         return 2*(x_i - min(x))/(max(x) - min(x)) - 1
 
-    # Expects x to already have been put through the sigmoid function
-    def sigmoid_prime(self, x):
-        return (1 + x)*(1 - x)
+    def denormalize(self, x, x_i):
+        return .5*(x_i - min(x))/(max(x) - min(x)) + 1
+
+    
 
     def relu(self, x):
         if x < -2 or x > 2:
             return 0
         return x 
-        
-
-    def bounded(x, low=-2, hi=2):
-        return min([max([-2, x]), 2])
-
-    def distance(self, y, t):
-        return (t - y)**2
-        # return sum([(t[i] - y[i])**2 for i in range(len(y))])
 
     def feed_forward(self, x):
         # print(x)
@@ -66,40 +66,49 @@ class RNN (object):
             z_in = self.v[0][j]
             z_in += sum([x[i-1]*self.v[i][j] for i in range(1, self.n + 1)])
             # Normalize Z_in to bring scale between 
-            # z_in = self.normalize(x, z_in)
+            z_in = self.normalize(x, z_in)
             # Apply activation function
             z_j = self.sigmoid(z_in)
             # z_j = z_in
             # Append to Z in order to broadcast to next layer
             z.append(z_j)
+        # Hidden State Interaction
+        # print(z)
+        h_t = np.add(z, np.dot(self.u, self.h))
+        h_t = [self.sigmoid(x) for x in h_t]
+        # h_t = np.dot(self.u, self.h)
+        # print('ht')
+        # print(h_t)
         y = []
         # Forward through outputs
         for k in range(self.m):
             y_in = self.w[0][k]
-            y_in += sum([z[j-1]*self.w[j][k] for j in range(1, self.p + 1)])
+            y_in += sum([h_t[j-1]*self.w[j][k] for j in range(1, self.p + 1)])
             # Normalize Y_in
-            # z_in = self.normalize(z, y_in)
+            z_in = self.normalize(z, y_in)
             # Apply activation function
             y_j = self.sigmoid(y_in)
             # y_j = y_in
             y.append(y_j)
         # Return position data from hidden & output layers
-        return z, y
+        return z, y, h_t
 
     def train(self, training_set, time_steps, learning_rate=0.05, epochs=1000, window=4):
         total_error = []
         for epoch in range(epochs):
             epoch_error = 0
             # The initial input value is the starting position
-            x = training_set[0]
+            x = training_set[0:window]
             # Weight corrections are computed and then averaged over the # of timesteps
             delta_w = [[0 for j in range(self.m)] for i in range(self.p + 1)]
             delta_v = [[0 for j in range(self.p)] for i in range(self.n + 1)]
+            delta_h = [[0 for j in range(self.p)] for i in range(self.p)]
             # For each time step
             for time in range(window, time_steps):
                 # Feedforward (passes X to the Hidden Layer)
-                z, y = self.feed_forward(training_set[time-window:time])
-                # z_t, y_t = self.feed_forward(training_set[time])
+                z, y, h_t = self.feed_forward(x)
+                # Update stored hidden values
+                self.h = h_t
                 # Set the next input to be the output at the previous timestep
                 x = training_set[time-window:time]
                 # print(y)
@@ -114,7 +123,7 @@ class RNN (object):
                 for k in range(self.m): 
                     epoch_error += (t - y[0])**2
                     # error_k = self.distance(y[k], t[k])
-                    error_k = ((t - y[k])**2)*self.sigmoid_prime(y[k])
+                    error_k = ((t - y[k]))*self.sigmoid_prime(y[k])
                     # Calculate weight correction terms
                     for j in range(1, self.p + 1):
                         delta_w[j][k] += learning_rate*error_k*z[j-1]
@@ -134,15 +143,29 @@ class RNN (object):
                     for i in range(1, self.n + 1):
                         delta_v[i][j - 1] += learning_rate*error_j*x[i-1]
                     delta_v[0][j - 1] += learning_rate*error_j
-
-            # Average Weight corrections by timestamps
-            delta_w = np.divide(delta_w, time_steps)
-            delta_v = np.divide(delta_v, time_steps)
+                
+                # Step 8 - Distribute error across Hidden Layer Weights
+                for j in range(1, self.p):
+                    error_in = error[0]*self.w[j][0]
+                    error_j = error_in * self.sigmoid_prime(z[j-1])
+                    # Weight correction term for hidden-to-hidden
+                    for l in range(1, self.p):
+                        delta_h[l][j-1] += learning_rate*error_j*h_t[l-1]
+                    delta_h[0][j-1] += learning_rate*error_j
             # Step 8 - Update Weights after all timesteps
+            #          Weights are batch updated
+            # Average Weight corrections by timestamps
+            # print(delta_w)
+            # delta_w = np.divide(delta_w, time_steps)
+            # delta_v = np.divide(delta_v, time_steps)
+            # delta_h = np.divide(delta_h, time_steps)
+            delta_w = np.clip(delta_w, -10, 10)
+            delta_v = np.clip(delta_v, -10, 10)
+            delta_h = np.clip(delta_h, -10, 10)
             self.w = np.add(self.w, delta_w)
             self.v = np.add(self.v, delta_v)
-            # if epoch > 10 and abs(epoch_error/time_steps - total_error[-1]) < 0.001:
-            #     break
+            self.h = np.add(self.h, delta_h)
+
             print('Epoch', epoch, '\tMSE', epoch_error/time_steps)
             total_error.append(epoch_error/time_steps)
 
--- tests.py
+++ tests.py
@@ -1,79 +1,23 @@
+# Set Up Imports
 from numpy.lib.type_check import real
 from util import *
 from physics_pendulum import *
 from make_plot import *
 from network import RNN
-# Retrieve test data
-# real_data = unpack_csv('./dataset/dpc_csv/0.csv')
-# network_input = read_csv_for_network('./dataset/test.csv')
-# print(network_input[:5])
-# scaled_data = scale_data(real_data)
 
-# start_pos = [0.004591383917239612,-0.008319249714070765,-0.41292958532786755,1.0373471062971127,0.44447954794333483,0.9665176561573177]
-# physics_data = list(physics_model(1, 1, network_input[0]))
-# physics_data.insert(0, [0 for i in range(len(physics_data[0]))])
-# physics_data.insert(0, [0 for i in range(len(physics_data[0]))])
-# make_plot(*physics_data)
-# make_plot(*scaled_data)
-# write_csv(physics_data, './dataset/physics_model/0.csv')
-# write_csv(scaled_data,  './dataset/dpc_scaled/0.csv')
+random.seed(231)
+# random.seed(420)
 
-# Retrieve training data
+learning_rate = 0.001
+time_steps = 20
+window = 4
+epochs = 500
 
-# training_set = read_csv_for_network('./dataset/physics_model/0_cropped.csv')
-# # training_set = unpack_csv('./dataset/physics_model/0.csv')
-# print(training_set[0])
-
-# # Train the Network on Physics Generated Dataset
-# network = RNN(2, 20, 2)
-# network.nguyen_widrow()
-# print(len(training_set))
-
-# # error = network.train(training_set, len(training_set), epochs=100)
-# # print(error)
-# print(np.array(network.w))
-# print(np.array(network.v))
-# plt.plot(training_set[4])
-# plt.ylabel('Error')
-# plt.xlabel('# of Epochs')
-# # plt.ylim([0, 10])
-# plt.show()
-
-training_set = read_csv_for_network('./dataset/physics_model/0_cropped.csv')[:600]
-training_set = [row[2] for row in training_set]
-
-# Clip Into 6 training sets
-training_set = [training_set[i*100:i*100+100] for i in range(6)]
-training_set = [[[x] for x in group] for group in training_set]
-print(len(training_set[0]))
-# print(training_set)
-x_network = RNN(1, 100, 1)
+# Initialize Network with Nguyen_Widrow weighting
+x_network = RNN(window, 20, 1)
 x_network.nguyen_widrow()
-x_network.n
-print(x_network.n)
-print(x_network.v[0][1])
+
 # Train Network on First Training Set
-error = x_network.train(training_set[0], 100)
-plt.plot(error)
-plt.ylabel('Error')
-plt.xlabel('# of Epochs')
-plt.show()
-"""
-# print(np.array(network.w))
-
-# Generate Output from Trained Network On Singular Dataset
-network_output = [[] for i in range(6)]
-x = training_set[0]
-for t in range(1, len(training_set)):
-    _, y = network.feed_forward(training_set[t])
-    x = y
-    # print(y)
-    network_output[0].append(y[0])
-    network_output[1].append(y[1])
-    network_output[2].append(y[2])
-    network_output[3].append(y[3])
-    network_output[4].append(y[4])
-    network_output[5].append(y[5])
-
-# write_csv(network_output, './dataset/results/0.csv')
-make_plot(*network_output)"""
+training_set = [float(2*(np.sin(.05*x))) for x in range(time_steps)]
+error = x_network.train(training_set[:time_steps], time_steps, \
+        learning_rate=learning_rate, epochs=epochs, window=window)
